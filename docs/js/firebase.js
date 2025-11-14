7f03c4049fe04ca022f71e35dcb7e36a7936d173
stayfuntaxcol <stephan.tollenaar@gmail.com>
2025-11-14 22:00:20 +0100

    Refactor Firebase import statements and logging

--- docs/js/firebase.js
+++ docs/js/firebase.js
@@ -1,9 +1,12 @@
-// LOG om te checken of dit bestand geladen wordt
 console.log("firebase.js geladen");
 
-// VERSIE HIER VERVANGEN door jouw versie uit de Firebase snippet
+// vervang 10.12.4 door de versie uit je Firebase CDN-snippet indien anders
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import {
+  getAuth,
+  signInAnonymously,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB_u6nKuM0JUv6lLksiAmExiEB3_wrCthA",
@@ -15,10 +18,9 @@ const firebaseConfig = {
   measurementId: "G-Y2SWPY1QZE"
 };
 
-const app  = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Aanroepen in index/host/player: initAuth(user => { ... })
 export function initAuth(onReady) {
   signInAnonymously(auth).catch((err) => {
     console.error("Anon sign-in fout:", err);
