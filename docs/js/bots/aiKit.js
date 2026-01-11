2dfa303c80013a278a86737514c2bf38a9a07b7d
stayfuntaxcol <stephan.tollenaar@gmail.com>
2026-01-11 14:12:56 +0100

    Create aiKit.js

---
+++ docs/js/bots/aiKit.js
@@ -0,0 +1,3 @@
+// docs/js/bots/aiKit.js
+export { RULES_INDEX, buildRulesIndex, getEventFacts, getActionFacts } from "./rulesIndex.js";
+export { BOT_WEIGHTS, scoreEventFacts, scoreActionFacts, rankActions } from "./botHeuristics.js";
